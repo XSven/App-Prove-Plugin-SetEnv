ed55bc3739d40b6c6d9678740e2f0102ac5eb136
Sven Willenbuecher <sven.willenbuecher@gmx.de>
2025-09-07 14:10:43 +0200

    Change authoring boilerplate

--- lib/App/Prove/Plugin/SetEnv.pm
+++ lib/App/Prove/Plugin/SetEnv.pm
@@ -1,9 +1,9 @@
-package App::Prove::Plugin::SetEnv;
-
-# ABSTRACT: a prove plugin to set environment variables
-
 use strict;
 use warnings;
+
+package App::Prove::Plugin::SetEnv;
+
+$App::Prove::Plugin::SetEnv::VERSION = 'v1.0.0'; ## no critic (ProhibitPackageVars)
 
 use String::Expand qw( expand_string );
 
@@ -11,30 +11,8 @@ sub load {
   my ( $class, $p ) = @_;
   foreach my $arg ( @{ $p->{ args } } ) {
     my ( $var, $val ) = split '=', $arg, 2;
-    $ENV{ $var } = expand_string( $val, \%ENV );
+    $ENV{ $var } = expand_string( $val, \%ENV ) ## no critic (RequireLocalizedPunctuationVars)
   }
 }
 
-=head1 SYNOPSIS
-
-    # command-line usage
-    prove -PSetEnv=TZ=UTC,PATH="./bin:$PATH"
-
-=head1 DESCRIPTION
-
-This L<prove> plugin lets you set environment variables for your test
-scripts.  It is particularly handy in C<.proverc>.
-
-=head1 BUGS
-
-Due to the way L<App::Prove> splits arguments to plugins, it is not
-possible to set values containing commas.
-
-=head1 SEE ALSO
-
-L<prove>, L<App::Prove/PLUGINS>.
-
-
-=cut
-
-1;
+1
--- t/basic.t
+++ t/basic.t
@@ -3,7 +3,7 @@ use warnings;
 
 use Test::More import => [ qw( is ) ], tests => 2;
 
-use App::Prove qw();
+use App::Prove ();
 
 my $default_PATH = '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin';
 local @ENV{ qw( FOO PATH ) } = ( '', $default_PATH );
@@ -11,5 +11,6 @@ my $app = App::Prove->new;
 $app->process_args( '-PSetEnv=FOO=bar,PATH=baz:$PATH' );
 $app->_load_extensions( $app->plugins, App::Prove::PLUGINS() );
 
-is $ENV{ FOO },  'bar',               'set new FOO environment variable';
-is $ENV{ PATH }, "baz:$default_PATH", 'expand string in PATH environment variable';
+is $ENV{ FOO }, 'bar', 'set new FOO environment variable';
+is $ENV{ PATH }, "baz:$default_PATH",
+  'expand string in PATH environment variable'
